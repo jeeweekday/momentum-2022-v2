e7fa218a9d976b0176e0d9b23e7948edef12fd46
jeeweekday <whops92@gmail.com>
2022-03-08 00:35:20 +0900

    Update todo JS

--- js/todo.js
+++ js/todo.js
@@ -1,11 +1,30 @@
 const todoForm = document.querySelector("#todo-form");
+const todoInput = todoForm.querySelector("input");
 const todoList = document.querySelector("#todo-list");
 
+const todoArr = [];
 
+function onSubmitTodo(event) {
+    event.preventDefault();
+    const whatToDo = todoInput.value;
+    const list = document.createElement("li");
+    todoList.appendChild(list);
+    const span1 = document.createElement("span");
+    list.appendChild(span1);
+    span1.innerText = whatToDo;
+    const span2 = document.createElement("span");
+    list.appendChild(span2);
+    span2.innerText = "V"
+    
+    todoArr.push(whatToDo);
+    localStorage.setItem("todo",JSON.stringify(todoArr));
+}
 
-const list = document.createElement("li");
-todoList.appendChild(list);
-const span1 = document.createElement("span");
-list.appendChild(span1);
-const span2 = document.createElement("span");
-list.appendChild(span2);
+savedTodo = localStorage.getItem("todo");
+if(savedTodo != null) {
+    const newTodo = JSON.parse(savedTodo);
+    console.log(newTodo);
+    //forEach()사용해야함.
+}
+
+todoForm.addEventListener("submit",onSubmitTodo);
