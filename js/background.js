c57bb987cba11e4555cb07380d39a55a50759dad
jeeweekday <whops92@gmail.com>
2022-03-11 00:39:13 +0900

    Update bg JS

--- js/background.js
+++ js/background.js
@@ -1,2 +1,7 @@
-const bg = document.createElement("img")
-bg.src = "./imgs/0.jpg"
+const bgArr = ["0.jpg"]//,"1.jpg","2.jpg"];
+const bgNum = Math.floor(Math.random() * bgArr.length);
+const bgPick = bgArr[bgNum]
+const bgBox = document.querySelector("#bg-box");
+const bgImg = document.createElement("img");
+bgImg.src = `imgs/${bgPick}`;
+bgBox.appendChild(bgImg);
