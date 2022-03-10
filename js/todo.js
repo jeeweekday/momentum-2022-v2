620cc67c36c98b8450120b6a35f36f6099230f09
jeeweekday <whops92@gmail.com>
2022-03-10 21:48:29 +0900

    Update todo JS

--- js/todo.js
+++ js/todo.js
@@ -7,24 +7,32 @@ const todoArr = [];
 function onSubmitTodo(event) {
     event.preventDefault();
     const whatToDo = todoInput.value;
+    todoInput.value = "";
+    paintTodo(whatToDo);
+}
+function paintTodo(todo) {
     const list = document.createElement("li");
     todoList.appendChild(list);
     const span1 = document.createElement("span");
     list.appendChild(span1);
-    span1.innerText = whatToDo;
+    span1.innerText = todo;
     const span2 = document.createElement("span");
     list.appendChild(span2);
     span2.innerText = "V"
+    // span2.addEventListener("click",deleteTodo);
+    span2.onclick = deleteTodo; //이렇게 해도 상관 없을까?
     
-    todoArr.push(whatToDo);
+    todoArr.push(todo);
     localStorage.setItem("todo",JSON.stringify(todoArr));
 }
-
-savedTodo = localStorage.getItem("todo");
-if(savedTodo != null) {
-    const newTodo = JSON.parse(savedTodo);
-    console.log(newTodo);
-    //forEach()사용해야함.
+function deleteTodo(event) {
+    event.target.parentElement.remove(); //this 사용 가능
 }
 
+const savedTodo = localStorage.getItem("todo");
+if(savedTodo != null) {
+    const newTodo = JSON.parse(savedTodo);
+    //forEach()사용해야함.
+    newTodo.forEach(paintTodo);
+}
 todoForm.addEventListener("submit",onSubmitTodo);
